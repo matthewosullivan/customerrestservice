e865c62cd4aa6a9c974d9ea58c1bfb41f6750397
matthewosullivan <mattosulli83@gmail.com>
2018-05-27 20:28:12 +1000

    swagger API Contract Definition

--- src/main/java/customer/CustomerApplication.java
+++ src/main/java/customer/CustomerApplication.java
@@ -3,8 +3,14 @@ package customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class CustomerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/customer/SpringfoxConfiguration.java
@@ -0,0 +1,17 @@
+package customer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringfoxConfiguration {
+  
+  @Bean
+  public Docket docket() {
+    return new Docket(DocumentationType.SWAGGER_2);
+  }
+  
+}
